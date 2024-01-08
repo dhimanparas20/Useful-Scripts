3c0d32f5ea9ac89f52bf5daa495c36464ba7c78a
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2024-01-08 12:31:49 +0530

    Create gitstorepassword.sh

---
+++ gitstorepassword.sh
@@ -0,0 +1,4 @@
+git config --global user.name "dhimanparas20"
+git config --global user.email "dhimanparas20@gmail.com"
+git config --global credential.helper cache
+git config --global credential.helper store
