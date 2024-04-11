109caa47ded28e6de5f07156da55b94470cdc361
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2024-04-11 23:00:46 +0530

    Update cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # Start Docker containers with fixed names
-docker stop mongoadminpannel && docker rm mongoadminpannel &&docker run -d --network=host --name mongoadminpannel mongoadminpannel 
-doker stop automation && docker rm automation &&docker run -d --network=host --name automation automation
+docker stop mongoadminpannel 
+docker rm mongoadminpannel 
+docker run -d --network=host --name mongoadminpannel mongoadminpannel 
+doker stop automation 
+docker rm automation 
+docker run -d --network=host --name automation automation
