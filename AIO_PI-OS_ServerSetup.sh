d06e027d4e6c5fac6360a1000d39b2f292390a4f
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2024-04-15 02:22:12 +0530

    Update AIO_PI-OS_ServerSetup.sh

--- AIO_PI-OS_ServerSetup.sh
+++ AIO_PI-OS_ServerSetup.sh
@@ -64,7 +64,6 @@ sleep 2
 
 #installing MongoDB
 clear
-delay 2000
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add - 
 echo "deb [ arch=arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 apt-get update --allow-insecure-repositories
