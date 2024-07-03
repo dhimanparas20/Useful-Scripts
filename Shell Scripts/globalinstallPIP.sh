4b4d8f56f67529ea692558b1687906a7b30fc784
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2024-07-03 16:55:42 +0530

    Update globalinstallPIP.sh

--- Shell Scripts/globalinstallPIP.sh
+++ Shell Scripts/globalinstallPIP.sh
@@ -1,1 +1,3 @@
 sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.old
+
+sudo mv /usr/lib/python3.12/EXTERNALLY-MANAGED /usr/lib/python3.12/EXTERNALLY-MANAGED.old
