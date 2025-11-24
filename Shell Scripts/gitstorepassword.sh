66a0cf3aa4f2e913c86a6e103b5e889e7497a457
paras <dhimanparas20@gmail.com>
2025-11-24 17:43:45 +0530

    Remove unused Docker and utility files.

--- Shell Scripts/gitstorepassword.sh
+++
@@ -1,4 +0,0 @@
-git config --global user.name "dhimanparas20"
-git config --global user.email "dhimanparas20@gmail.com"
-git config --global credential.helper cache
-git config --global credential.helper store
--- Shell Scripts/globalinstallPIP.sh
+++
@@ -1,3 +0,0 @@
-sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.old
-
-sudo mv /usr/lib/python3.12/EXTERNALLY-MANAGED /usr/lib/python3.12/EXTERNALLY-MANAGED.old
